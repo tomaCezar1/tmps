8df39cb743bb9c77ac988db2c94d182c04f1b268
Cezar <cezar.toma1@gmail.com>
2022-03-14 17:32:07 +0200

    facade pattern

---
+++ Lab2/facade/after.js
@@ -0,0 +1,26 @@
+function getUsers() {
+  return getFetch('https://jsonplaceholder.typicode.com/users');
+}
+
+function getUserPosts(userId) {
+  return getFetch('https://jsonplaceholder.typicode.com/posts', {
+    userId: userId,
+  });
+}
+
+function getFetch(url, params = {}) {
+  return axios({
+    url: url,
+    method: 'GET',
+    params: params,
+  }).then((res) => res.data);
+}
+
+getUsers().then((users) => {
+  users.forEach((user) => {
+    getUserPosts(user.id).then((posts) => {
+      console.log(user.name);
+      console.log(posts.length);
+    });
+  });
+});
