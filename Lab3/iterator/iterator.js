756e4bf9aeaaa7019e51edca46671ebb83847f74
Cezar <cezar.toma1@gmail.com>
2022-03-30 08:25:00 +0300

    updated iterator

--- Lab3/iterator/iterator.js
+++ Lab3/iterator/iterator.js
@@ -16,3 +16,30 @@ function range(start, end, step = 1) {
 for (const n of range(0, 20, 5)) {
   console.log(n);
 }
+
+function makeRange(start = 0, end = 100, step = 1) {
+  let nextIndex = start;
+  let iterationCount = 0;
+
+  const rangeIterator = {
+    next: function () {
+      let result;
+      if (nextIndex < end) {
+        result = { value: nextIndex, done: false };
+        nextIndex += step;
+        iterationCount++;
+        return result;
+      }
+      return { value: iterationCount, done: true };
+    },
+  };
+  return rangeIterator;
+}
+
+const iterator = makeRangeIterator(1, 10, 2);
+
+let result = iterator.next();
+while (!result.done) {
+  console.log(result.value); // 1 3 5 7 9
+  result = iterator.next();
+}
